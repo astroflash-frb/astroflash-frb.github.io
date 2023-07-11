7508991c998bc1c3cb544712239566a6afc5343a
Benito Marcote <benito.marcote@icloud.com>
2023-07-11 09:06:09 +0200

    Adding Ny & Dirk. More general .sh script

--- actions/parse_all.sh
+++ actions/parse_all.sh
@@ -1,4 +1,16 @@
 #! /usr/bin/env bash
-python3 $(dirname "$0")/parse_article.py
-python3 $(dirname "$0")/parse_teams.py
-python3 $(dirname "$0")/parse_index.py
+
+
+if [ $(basename "$PWD") != "actions" ];then
+    WD=..
+    cd actions
+else
+    WD=.
+fi
+
+python3 parse_article.py
+python3 parse_teams.py
+python3 parse_index.py
+
+cd $WD
+
